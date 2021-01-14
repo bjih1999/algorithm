349464b6f18f54e387261a6871c7e4e9cb9b5606
Jihyun <bjih1999@naver.com>
2021-01-14 20:14:07 +0900

    전화번호 목록 정답 개선

--- programmers/전화번호 목록/Solution.java
+++ programmers/전화번호 목록/Solution.java
@@ -14,12 +14,12 @@ public class Solution {
 	public boolean solution(String[] phone_book) {
 		boolean answer = true;
 	
-		for(int i = 0 ; i < phone_book.length ; i++){
+		L1 : for(int i = 0 ; i < phone_book.length - 1 ; i++){
 			for(int j = i+1 ; j < phone_book.length ; j++){
 				if(phone_book[i].charAt(0) == phone_book[j].charAt(0)){
 					if(phone_book[i].startsWith(phone_book[j]) || phone_book[j].startsWith(phone_book[i])){
 						answer = false;
-						break;
+						break L1;
 					}
 				}
 			}
