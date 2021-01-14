851d346fdf1dc60202c0b176e3cd077f9d51bcf0
Jihyun <bjih1999@naver.com>
2021-01-14 18:43:27 +0900

    124 나라의 숫자 정답

--- programmers/124 나라의 숫자/Solution.java
+++ programmers/124 나라의 숫자/Solution.java
@@ -1,40 +1,60 @@
-import java.util.ArrayList;
-
 class Solution {
 	public String solution(int n) {
 		String answer = "";
-
 		StringBuilder sb = new StringBuilder();
-		ArrayList<String> arr = new ArrayList<>();
-		int quotient = 0;
 		int remainder = 0;
-		int digit1 = 0;
-		arr.add("0");
-		for(int i = 1 ; i <= n ; i++){
-			quotient = i / 3;
-			remainder = i % 3;
-			if(remainder == 0){
-				digit1 = 4;
-			}
-			else{
-				digit1 = remainder;
-			}
 
+		while(n > 0){			
+			remainder = n%3;
+			n = n/3;
 			if(remainder == 0){
-				sb.append(arr.get(quotient-1));
-				sb.append(digit1);
-				arr.add(i, sb.toString());
-				sb = new StringBuilder();
+				n = n - 1;
+				remainder = 4;
 			}
-			else{
-				sb.append(arr.get(quotient));
-				sb.append(digit1);
-				arr.add(i, sb.toString());
-				sb = new StringBuilder();
-			}
+			sb.insert(0, remainder);
 		}
-		answer = sb.append(Long.parseLong(arr.get(n))).toString();
-		sb = new StringBuilder();
+		answer = sb.toString();
 		return answer;
 	}
 }
+
+// import java.util.ArrayList;
+
+// class Solution {
+// 	public String solution(int n) {
+// 		String answer = "";
+
+// 		StringBuilder sb = new StringBuilder();
+// 		ArrayList<String> arr = new ArrayList<>();
+// 		int quotient = 0;
+// 		int remainder = 0;
+// 		int digit1 = 0;
+// 		arr.add("0");
+// 		for(int i = 1 ; i <= n ; i++){
+// 			quotient = i / 3;
+// 			remainder = i % 3;
+// 			if(remainder == 0){
+// 				digit1 = 4;
+// 			}
+// 			else{
+// 				digit1 = remainder;
+// 			}
+
+// 			if(remainder == 0){
+// 				sb.append(arr.get(quotient-1));
+// 				sb.append(digit1);
+// 				arr.add(i, sb.toString());
+// 				sb = new StringBuilder();
+// 			}
+// 			else{
+// 				sb.append(arr.get(quotient));
+// 				sb.append(digit1);
+// 				arr.add(i, sb.toString());
+// 				sb = new StringBuilder();
+// 			}
+// 		}
+// 		answer = sb.append(Long.parseLong(arr.get(n))).toString();
+// 		sb = new StringBuilder();
+// 		return answer;
+// 	}
+// }
--- programmers/124 나라의 숫자/main.java
+++ programmers/124 나라의 숫자/main.java
@@ -9,6 +9,7 @@ public class main {
 		System.out.println("14 " +solution.solution(14));
 		System.out.println("15 " +solution.solution(15));
 		System.out.println("17 " +solution.solution(17));
+		System.out.println("18 " +solution.solution(18));	
 		System.out.println("301 " +solution.solution(301));
 		
 		System.out.println("100000 " +solution.solution(100000));
