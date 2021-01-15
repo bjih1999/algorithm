00d00ad2e25eb58828e0d878994d78df0ee1d293
Jihyun <bjih1999@naver.com>
2021-01-16 02:30:57 +0900

    베스트 앨범 rearrange

--- programmers/베스트앨범/src/main.java
+++
@@ -1,8 +0,0 @@
-public class main {
-    public static void main(String[] agrs){
-        Solution solution = new Solution();
-        String[] genres = {"classic", "pop", "classic", "classic", "pop"};
-        int[] plays = {500, 600, 150, 800, 2500};
-        solution.solution(genres, plays);
-    }
-}
