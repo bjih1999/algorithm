2801bce6a0e21b788cf2b0a853e729b163e41cb0
Jihyun <bjih1999@naver.com>
2021-01-18 01:12:59 +0900

    네트워크 정답

--- programmers/DFS_BFS/타겟 넘버/solution.py
+++ programmers/DFS_BFS/타겟 넘버/solution.py
@@ -12,7 +12,7 @@ https://programmers.co.kr/learn/courses/30/lessons/43165
 
 import copy
 
-def search(numbers, index, target, answer):
+def _search(numbers, index, target, answer):
     number_arr = copy.deepcopy(numbers)
     if index > len(number_arr)-2 :
         if sum(number_arr[:-1]) + number_arr[-1] == target:
@@ -31,6 +31,6 @@ def search(numbers, index, target, answer):
 
 def solution(numbers, target):
     answer = [0]
-    search(numbers, 0, target, answer)
+    _search(numbers, 0, target, answer)
 
     return answer[0]
