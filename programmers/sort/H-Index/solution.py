5e38ddf50af6f4e72a88f4a6a6d122b427bd5244
Jihyun <bjih1999@naver.com>
2021-01-21 01:21:20 +0900

    H-Index 정답

--- programmers/sort/H-Index/solution.py
+++ programmers/sort/H-Index/solution.py
@@ -8,7 +8,3 @@ def solution(citations):
             answer = count + 1
 
     return answer
-
-print(solution([3, 0, 6, 1, 5]))
-print(solution([100, 96]))
-print(solution([47, 42, 32, 28, 24, 22, 17, 15, 10, 8]))
