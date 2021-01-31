fa308570836f23b6a0e1959420367d776b69d161
Jihyun <bjih1999@naver.com>
2021-01-31 16:14:49 +0900

    가장 먼 노드 다익스라 버전

---
+++ programmers/graph/가장 먼 노드/solution_ver_dijkstra.py
@@ -0,0 +1,52 @@
+import heapq
+
+def solution(n, edge):
+    answer = 0
+    not_visited = [i for i in range(1, n+1)]
+    adj_list = [{} for i in range(n+1)]
+    distance = []
+    
+    not_visited.remove(1)
+
+    for index, path in enumerate(edge):
+        edge[index] = sorted(path, key=lambda x:x)
+    sorted_edge = sorted(edge, key=lambda x:(x[0], x[1]))
+    # print(sorted_edge)
+
+    for i in range(1, n+1):
+        adj_list[i][i] = 0
+    for path in edge:
+        adj_list[path[0]][path[1]] = 1
+        adj_list[path[1]][path[0]] = 1
+    
+    distance = adj_list[1]
+    # print('distance : ',distance)
+    while len(not_visited) > 0:
+        min_list = []
+        # print('distance : ', distance)
+        for node in not_visited:
+            # print(node)
+            # print('not_visited : ',not_visited)
+            # print('distance : ', distance)
+            if node in distance:
+                heapq.heappush(min_list, (distance[node], node))
+                # print(min_list)
+        # print('---------------')
+        cur_visit = min_list[0]
+        not_visited.remove(cur_visit[1])
+
+        for node in adj_list[cur_visit[1]].keys():
+            if node not in distance:
+                distance[node] = distance[cur_visit[1]] + adj_list[cur_visit[1]][node]
+        # print('distance : ',distance[1:])
+    most_far_length = max(distance.values())
+    # print(distance)
+    # print(most_far_length)
+    # print(distance.keys())
+    for node in distance.keys():
+        if most_far_length == distance[node]:
+            answer += 1
+
+    return answer
+
+print(solution(6, 	[[3, 6], [4, 3], [3, 2], [1, 3], [1, 2], [2, 4], [5, 2]]))
