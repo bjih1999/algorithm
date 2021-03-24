109856387f97cb69ddfd22d2ee7ced9d40b5d31b
Jihyun <bjih1999@naver.com>
2021-03-24 23:55:40 +0900

    작성중

---
+++ BOJ/class3/듣보잡/1764.py
@@ -0,0 +1,18 @@
+import sys
+
+N, M = list(map(int, sys.stdin.readline().rstrip().split()))
+
+heardx = []
+sawx = []
+
+for _ in range(N):
+	heardx.apped(sys.stdin.readline().rstrip())
+
+for _ in range(M):
+	sawx.apend(sys.stdin.readline().rstrip())
+
+heardx = sorted(heardx)
+sawx = sorted(sawx)
+
+# for hname in heardx:
+# 	for sawx	
