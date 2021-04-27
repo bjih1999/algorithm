257dcfd8ec5e27fc45cfde1dbfbbd01dff8b3fec
Jihyun <bjih1999@naver.com>
2021-04-27 23:20:36 +0900

    집합 메모리 초과

--- BOJ/class3/집합/11723.py
+++ BOJ/class3/집합/11723.py
@@ -4,30 +4,29 @@ import math
 M = int(sys.stdin.readline().rstrip())
 
 S = 0
-output = []
+answer = ''
 for _ in range(M):
 	instruction = list(sys.stdin.readline().rstrip().split())
 	if len(instruction) == 2:
 		x = instruction[1]
 	operator = instruction[0]
 	x = int(x)
-	target_bit = 1<<(x-1)
-	S = int(S)
+	target_bit = x-1
 	if operator == 'add':
 		S |= 1<<target_bit
 	elif operator == 'remove':
 		S &= ~(1<<target_bit)
 	elif operator == 'check':
-		if S & 1<<target_bit == 0:
-			output.append(0)
+		if S>>target_bit & 1 == 0:
+			answer += '0'
 		else:
-			output.append(1)
+			answer += '1'
+		answer += '\n'
 	elif operator == 'toggle':
 		S ^= 1<<target_bit
 	elif operator == 'all':
-		S = math.pow(2, 21)-1
+		S = 2**20-1
 	elif operator == 'empty':
 		S = 0
 
-for x in output:
-	print(x)
+print(answer.rstrip())
