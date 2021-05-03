7a5c66718cf0daf3f43cd7bdf527f4ab72da87ab
Jihyun <bjih1999@naver.com>
2021-05-03 23:18:24 +0900

    집합

--- BOJ/class3/집합/11723.py
+++ BOJ/class3/집합/11723.py
@@ -3,29 +3,33 @@ import math
 
 M = int(sys.stdin.readline().rstrip())
 
-S = 0
-answer = ''
+S = set()
+answers = ''
 for _ in range(M):
 	target_bit = 0
-	instruction = list(sys.stdin.readline().rstrip().split())
+	instruction = sys.stdin.readline().rstrip().split()
 	if len(instruction) > 1:
-		target_bit = int(instruction[1]) - 1
+		value = int(instruction[1])
 
 	if instruction[0] == 'add':
-		S |= 1<<target_bit
+		S.add(value)
 	elif instruction[0] == 'remove':
-		S &= ~(1<<target_bit)
+		if value in S:
+			S.remove(value)
 	elif instruction[0] == 'check':
-		if S>>target_bit & 1 == 0:
-			answer += '0'
+		if value in S:
+			answers+='1\n'
 		else:
-			answer += '1'
-		answer += '\n'
+			answers+='0\n'
 	elif instruction[0] == 'toggle':
-		S ^= 1<<target_bit
+		if value in S:
+			S.remove(value)
+		else:
+			S.add(value)
 	elif instruction[0] == 'all':
-		S = 2**20-1
+		for i in range(21):
+			S.add(i)
 	elif instruction[0] == 'empty':
-		S = 0
+		S.clear()
 
-print(answer.rstrip())
+print(answers.rstrip())
