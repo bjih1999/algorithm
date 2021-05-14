8ae67ac09227d1f71f5e80a30e4c384b52b052b3
Jihyun <bjih1999@naver.com>
2021-05-14 20:37:04 +0900

    좌표 압축 정답

---
+++ BOJ/class3/좌표 압축/18870.py
@@ -0,0 +1,24 @@
+import sys
+from collections import deque
+from types import coroutine
+
+N = int(sys.stdin.readline())
+X = list(map(int, sys.stdin.readline().rstrip().split()))
+
+answer = deque()
+
+sortedX = enumerate(sorted(list(set(X))))
+# print(sorted(list(set(X))))
+coordinate = {}
+
+for pos, x in sortedX:
+	coordinate[x] = pos
+
+answers = []
+for x in X:
+	answers.append(coordinate[x])
+
+# print(coordinate)
+print(' '.join(map(str, answers)))
+
+
