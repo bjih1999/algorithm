96143eadcfdfd3f7370f8760558d4fbc484d01c2
Jihyun <bjih1999@naver.com>
2021-05-15 16:35:21 +0900

    연결 요소의 개수 정답

---
+++ BOJ/class3/연결 요소의 개수/11724.py
@@ -0,0 +1,29 @@
+import sys
+from collections import deque
+
+N, M = list(map(int, sys.stdin.readline().rstrip().split()))
+
+adjList = {}
+notVisited = set(i for i in range(1, N+1))
+for i in range(1, N+1):
+    adjList[i] = set()
+
+for _ in range(M):
+    a, b =  list(map(int, sys.stdin.readline().rstrip().split()))
+    adjList[a].add(b)
+    adjList[b].add(a)
+
+count = 0
+queue = deque()
+while len(notVisited) > 0:
+    queue.append(min(notVisited))
+    while len(queue) != 0:
+        node = queue.popleft()
+        if node in notVisited:
+            notVisited.remove(node)
+        for i in adjList[node]:
+            if i in notVisited and i not in queue:
+                queue.append(i)
+        print(queue)
+    count += 1
+print(count)
