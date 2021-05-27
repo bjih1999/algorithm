1c1dd3219c4346bada20283d71b0e792c99308eb
Jihyun <bjih1999@naver.com>
2021-05-27 23:00:39 +0900

    등굣길 정답

--- programmers/level3/등굣길/solution.py
+++ programmers/level3/등굣길/solution.py
@@ -2,29 +2,21 @@
 
 def solution(m, n, puddles):
     map = []
-    map.append([1 for _ in range(m)])
 
-    for _ in range(1, n):
+    for _ in range(n):
         row = [0 for _ in range(m)]
-        row[0] = 1
         map.append(row)
+    map[0][0] = 1
     print(map)
     for i in range(n):
         for j in range(m):
-            up = 0
-            right = 0
-            print(i, j)
-            # print([i+1, j+1] in puddles)
-            if [i+1, j+1] in puddles:
-                # print(i+1, j+1)
-                map[i][j] = 0
-            else:
-                if 1 < i < n:
-                    up = map[i-1][j]
-                if 0 < j < m-1:
-                    right = map[i][j+1]
-                map[i][j] = up + right
-        print(map)
+            if [j+1, i+1] not in puddles:
+                map[i][j]
+                if 0 < i < n:
+                    map[i][j] += map[i-1][j]
+                if 0 < j < m:
+                    map[i][j] += map[i][j-1]
+    print(map)
     return map[n-1][m-1]
 
-print(solution(4, 3, [[2, 2]]))
+# print(solution(7, 4, [[2, 1], [2, 2], [2, 3], [4, 2], [4, 3], [4, 4], [6, 2], [6, 3]]))
