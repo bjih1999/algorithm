cbcd9d5c0ac5adbb51b0eaebc9a887c473d0e108
bjih1999 <bjih1999@naver.com>
2023-11-07 23:59:20 +0900

    1475 정답

---
+++ BOJ/baaaaaaaakingdog/배열/1475.py
@@ -0,0 +1,18 @@
+import sys
+import math
+# number = sys.stdin.readline().rstrip()
+
+number = '9999'
+count = { n:0 for n in range(0, 9)}
+
+for n in number:
+    n = int(n)
+    if n == 9 or n == 6:
+        count[6] += 0.5
+    else:
+        count[n] += 1
+
+count[6] = int(math.ceil(count[6]))
+maximum = max(count.values())
+
+print(maximum)
