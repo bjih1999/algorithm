2cbeed62eb55a768d4886a0bc4a30339d5442564
bjih1999 <bjih1999@naver.com>
2024-01-13 16:26:00 +0900

    종이의 개수 정답

--- BOJ/baaaaaaaakingdog/재귀/1780.py
+++ BOJ/baaaaaaaakingdog/재귀/1780.py
@@ -1,18 +1,5 @@
 import sys
 
-sys.setrecursionlimit(10 ** 6)
-n = int(sys.stdin.readline().rstrip())
-
-board = []
-answer = {
-    -1: 0,
-    0: 0,
-    1: 0,
-}
-
-for _ in range(n):
-    board.append(list(map(int, sys.stdin.readline().rstrip().split())))
-
 def check(x, y, n):
     global board    
     for i in range(x, x + n):
@@ -30,7 +17,20 @@ def func (x, y, k):
 
     for i in range(3):
         for j in range(3):
-            func(x + k/3 * i, y + k/3 * j, k/3)
+            func(x + k//3 * i, y + k//3 * j, k//3)
+
+sys.setrecursionlimit(10 ** 6)
+n = int(sys.stdin.readline().rstrip())
+
+board = []
+answer = {
+    -1: 0,
+    0: 0,
+    1: 0,
+}
+
+for _ in range(n):
+    board.append(list(map(int, sys.stdin.readline().rstrip().split())))
  
 func(0, 0, n)
 print(answer[-1])
