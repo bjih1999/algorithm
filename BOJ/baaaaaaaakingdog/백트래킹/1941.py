ffe60f4be7c1f28384f484f4b45ab61b1a87f4c6
bjih1999 <bjih1999@naver.com>
2024-01-31 23:58:47 +0900

    소문난 칠공주

--- BOJ/baaaaaaaakingdog/백트래킹/1941.py
+++ BOJ/baaaaaaaakingdog/백트래킹/1941.py
@@ -16,31 +16,41 @@ def promising(i, j):
     if i < 0 or i >= 5 or j < 0 or j >= 5:
         return False
     
-    if (board[i][j] == 'Y' and y <= 3):
+    if (board[i][j] == 'Y' and s >= 3):
         return False
 
     return True
 
 def func(i, j):
-    global board, answer, y, s
+    global board, answer, y, s, moves
+
     if board[i][j] == 'Y':
         y += 1
     else:
         s += 1
     
     if y + s == 7:
-        print(y, s)
+        for v in visited:
+            print(v)
         answer += 1
 
+    for move in moves:
+        ni, nj = i + move[0], j + move[1]
+        if (0 <= ni < 5 and 0 <= nj < 5) and not visited[ni][nj]:
+            if board[ni][nj] == 'Y' and y > 3:
+                continue
+            visited[ni][nj] = True
+            if board[ni][nj] == 'Y':
+                y += 1
+            else:
+                s += 1
+            func(ni, nj)
+            visited[ni][nj] = False
+            if board[ni][nj] == 'Y':
+                y -= 1
+            else:
+                s -= 1
 
-for i in range(5):
-    for j in range(5):
-        for move in moves:
-            ni, nj = i + move[0], j + move[1]
-
-            if promising(ni, nj) and not visited[ni][nj]:
-                visited[ni][nj] = True
-                func(ni, nj)
-                visited[ni][nj] = False
+func(0, 0)
 
 print(answer)
