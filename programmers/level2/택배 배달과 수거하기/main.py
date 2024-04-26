3a419da0fafefc9a2c095d4f129b4592ce2218ef
bjih1999 <bjih1999@naver.com>
2024-04-26 17:51:16 +0900

    택배 배달과 수거하기 정답

--- programmers/level2/택배 배달과 수거하기/main.py
+++ programmers/level2/택배 배달과 수거하기/main.py
@@ -1,37 +1,24 @@
 # -*- coding: utf-8 -*-
 
 def solution(cap, n, deliveries, pickups):
-    
-    # 가장 마지막 집 찾기
-    cur = n - 1
-    while not deliveries[cur] and not pickups[cur]:
-        cur -= 1
+    answer = 0
 
-    distance = 0
+    deliveries = deliveries[::-1]
+    pickups = pickups[::-1]
+
+    delivery = 0
+    pickup = 0
+
+    for i in range(n):
+        delivery += deliveries[i]
+        pickup += pickups[i]
     
-    cur_cap = 0
-    carrying = 0
-    last = 0
-    while cur >= 0:
-        last = cur
-        while True:
-            if cur < 0:
-                break
-            
-            if cur_cap + deliveries[cur] > cap or carrying + pickups[cur] > cap:
-                distance += (last + 1) * 2
-                cur_cap = 0
-                carrying = 0
-                print(last + 1)
-                break
-            
-            cur_cap += deliveries[cur]
-            carrying + pickups[cur]
-            
-            cur -= 1
+        while delivery > 0 or pickup > 0:
+            delivery -= cap
+            pickup -= cap
+            answer += (n-i) * 2
     
-    distance += (last + 1) * 2
-    return distance
+    return answer
 
 print(solution(4, 5, [1, 0, 3, 1, 2], [0, 3, 0, 4, 0]))
 print(solution(2, 7, [1, 0, 2, 0, 1, 0, 2], [0, 2, 0, 1, 0, 2, 0]))
