c4dd2a5b9fa42251f4bb64d46a8423b946a2a947
bjih1999 <bjih1999@naver.com>
2024-05-16 23:58:10 +0900

    로또 별해

---
+++ BOJ/baaaaaaaakingdog/백트래킹/6603_2.py
@@ -0,0 +1,30 @@
+import sys
+
+k = 0
+s = 0
+result = [0 for _ in range(6)]
+def dfs(depth, cur):
+    global s, result
+    if depth == 6:
+        print(' '.join(list(map(str, result))))
+        return
+    
+    for i in range(cur, k):
+        result[depth] = s[i]
+        dfs(depth+1, cur+1)
+
+question = []
+while True:
+    input = sys.stdin.readline().rstrip()
+    if input == '0':
+        break
+    
+    question.append(input)
+
+
+for q in question:
+    temp = list(map(int, q.split()))
+    k = temp[0]
+    s = sorted(temp[1:])
+    dfs(0, 0)
+    print('')
