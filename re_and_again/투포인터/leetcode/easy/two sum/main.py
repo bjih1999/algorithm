82cc7e02dc27d76ce62e2400cc3778a46f04257c
bjih1999 <bjih1999@naver.com>
2024-12-01 00:09:28 +0900

    이분탐색:leetcode-easy:two sum

--- re_and_again/투포인터/leetcode/easy/two sum/main.py
+++ re_and_again/투포인터/leetcode/easy/two sum/main.py
@@ -2,8 +2,8 @@
 nums의 두 수를 골라 target을 만들 수 있는 수의 인덱스를 리턴하는 문제
 2 <= nums <= 10**4
 
-투 포인터를 활용해 O(n)으로 풀이 가능
-
+투 포인터를 활용해 O(n)으로 풀이 가능.
+반드시 해가 1개 존재하기 때문에 예외처리 없이 풀이 가능.
 '''
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
